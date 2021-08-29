ae762c1c95ff581593f901dc6ac6311ca118752f
xztxy <31698398+xztxy@users.noreply.github.com>
2021-08-29 17:48:40 +0800

    Update ua.go

--- models/ua.go
+++ models/ua.go
@@ -7,7 +7,7 @@ import (
 	"github.com/beego/beego/v2/client/httplib"
 )
 
-var ua = "'jdapp;android;10.0.5;11;0393465333165363-5333430323261366;network/wifi;model/M2102K1C;osVer/30;appBuild/88681;partner/lc001;eufv/1;jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 11; M2102K1C Build/RKQ1.201112.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045534 Mobile Safari/537.36'"
+var ua = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_2 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8H7 Safari/6533.18.5 UCBrowser/13.4.2.1122"
 
 func initUserAgent() {
 	u := &UserAgent{}
