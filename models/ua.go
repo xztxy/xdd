a31c93eba2874c6e27dcc14b5a093469b80e9c2b
xztxy <31698398+xztxy@users.noreply.github.com>
2021-08-29 12:30:32 +0800

    Update ua.go

--- models/ua.go
+++ models/ua.go
@@ -7,7 +7,7 @@ import (
 	"github.com/beego/beego/v2/client/httplib"
 )
 
-var ua = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 SP-engine/2.14.0 main%2F1.0 baiduboxapp/11.18.0.16 (Baidu; P2 13.3.1) NABar/0.0"
+var ua = "Mozilla/5.0 (iPhone; CPU iPhone OS 14_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.11(0x18000b26) NetType/WIFI Language/zh_CN"
 
 func initUserAgent() {
 	u := &UserAgent{}
@@ -25,7 +25,7 @@ func initUserAgent() {
 		} else {
 			logs.Info("更新User-Agent")
 			var err error
-			ua, err = httplib.Get(GhProxy + "https://raw.githubusercontent.com/cdle/xdd/main/ua.txt").String()
+			ua, err = httplib.Get(GhProxy + "https://github.com/xztxy/xdd/blob/main/ua.txt").String()
 			if err != nil {
 				logs.Info("更新User-Agent失败")
 			}
